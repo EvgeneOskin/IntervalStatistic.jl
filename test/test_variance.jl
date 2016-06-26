a06bcb61c40bc950834e36f9b6064b17f278c077
EvgeneOskin <eoskin@crystalnix.com>
2016-06-26 17:09:16 +0600

    Refactor tests.

--- test/test_variance.jl
+++ test/test_variance.jl
@@ -7,78 +7,76 @@ using Distributions;
 
 srand(10)
 
-facts("estimate variance By confidence probability standard (0, 1) distribution") do
+contain(value) = (i) -> value in i
+
+facts("estimate variance of standard (0, 1) distribution") do
     d = Normal()
     length = 100
     values = rand(d, length)
     mu, sigma = params(d)
     average = reduce(+, values) / length
-    result = IntervalStatistic.Variance.byConfidenceProbability(average, values, 0.95, length)
-    println(result)
-    @fact sigma*sigma --> less_than(result.hi)
-    @fact sigma*sigma --> greater_than(result.lo)
+
+    context("by confidence probability") do
+        result = IntervalStatistic.Variance.byConfidenceProbability(average, values, 0.95, length)
+        println(result)
+        @fact result --> contain(sigma^2)
+    end
+
+    context("by point variance") do
+        variance = mapreduce((x) -> (x - average)^2, +, values) / length
+        result = IntervalStatistic.Variance.byPointVariance(variance, 0.95, length)
+        println(result)
+        @fact result --> contain(variance)
+    end
 end
 
-facts("estimate variance By confidence probability mu=3 sigma=0.1 distribution") do
-    d = Normal(3, 0.1)
-    length = 100
-    values = rand(d, length)
-    mu, sigma = params(d)
-    average = reduce(+, values) / length
-    result = IntervalStatistic.Variance.byConfidenceProbability(average, values, 0.95, length)
-    println(result)
-    @fact sigma*sigma --> less_than(result.hi)
-    @fact sigma*sigma --> greater_than(result.lo)
-end
-
-facts("estimate variance By point variance standard (0, 1) distribution") do
-    d = Normal()
-    length = 100
-    values = rand(d, length)
-    mu, sigma = params(d)
-    average = reduce(+, values) / length
-    variance = mapreduce((x) -> (x - average)^2, +, values) / length
-    result = IntervalStatistic.Variance.byPointVariance(variance, 0.95, length)
-    println(result)
-    @fact sigma*sigma --> less_than(result.hi)
-    @fact sigma*sigma --> greater_than(result.lo)
-end
-
-facts("estimate variance By point variance mu=3 sigma=0.1 distribution") do
-    d = Normal(3, 0.1)
-    length = 100
-    values = rand(d, length)
-    mu, sigma = params(d)
-    average = reduce(+, values) / length
-    variance = mapreduce((x) -> (x - average)^2, +, values) / length
-    result = IntervalStatistic.Variance.byPointVariance(variance, 0.95, length)
-    println(result)
-    @fact sigma*sigma --> less_than(result.hi)
-    @fact sigma*sigma --> greater_than(result.lo)
-end
-
-facts("estimate variance By mean absolute deviation standard (0, 1) distribution") do
-    d = Normal()
-    length = 10
-    values = rand(d, length)
-    mu, sigma = params(d)
-    average = reduce(+, values) / length
-    result = IntervalStatistic.Variance.byMeanAbsoluteDeviation(average, values, 0.95, length)
-    println(result)
-    @fact sigma*sigma --> less_than(result.hi)
-    @fact sigma*sigma --> greater_than(result.lo)
-end
-
-facts("estimate variance By mean absolute deviation mu=3 sigma=0.1 distribution") do
+facts("estimate variance of normanl mu=3 sigma=0.1 distribution") do
     d = Normal(3, 0.1)
     length = 10
     values = rand(d, length)
     mu, sigma = params(d)
     average = reduce(+, values) / length
-    result = IntervalStatistic.Variance.byMeanAbsoluteDeviation(average, values, 0.95, length)
-    println(result)
-    @fact sigma*sigma --> less_than(result.hi)
-    @fact sigma*sigma --> greater_than(result.lo)
+
+    context("by confidence probability") do
+        result = IntervalStatistic.Variance.byConfidenceProbability(average, values, 0.95, length)
+        println(result)
+        @fact result --> contain(sigma^2)
+    end
+
+    context("by point variance") do
+        variance = mapreduce((x) -> (x - average)^2, +, values) / length
+        result = IntervalStatistic.Variance.byPointVariance(variance, 0.95, length)
+        println(result)
+        @fact result --> contain(variance)
+    end
+end
+
+facts("estimate variance of standard (0, 1) distribution with 10 samples") do
+    d = Normal()
+    length = 10
+    values = rand(d, length)
+    mu, sigma = params(d)
+    average = reduce(+, values) / length
+
+    context("by mean abs deviation") do
+        result = IntervalStatistic.Variance.byMeanAbsoluteDeviation(average, values, 0.95, length)
+        println(result)
+        @fact result --> contain(sigma^2)
+    end
+end
+
+facts("estimate variance of normanl mu=3 sigma=0.1 distribution with 10 samples") do
+    d = Normal(3, 0.1)
+    length = 10
+    values = rand(d, length)
+    mu, sigma = params(d)
+    average = reduce(+, values) / length
+
+   context("by mean abs deviation") do
+        result = IntervalStatistic.Variance.byMeanAbsoluteDeviation(average, values, 0.95, length)
+        println(result)
+        @fact result --> contain(sigma^2)
+    end
 end
 
 end
